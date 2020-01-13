c52f77696e03e9528a98dcb4a15503f39f2cb98f
krishna kumar <kkumar@ztech.io>
2020-01-13 08:25:00 +0530

    theme swtch

--- src/components/Header/Theme.js
+++ src/components/Header/Theme.js
@@ -1,11 +1,32 @@
 import React, { Component } from 'react';
 
 export default class Theme extends Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            theme: 'dark',
+        };
+        this.toggleTheme = this.toggleTheme.bind(this);
+        // this.toggleTheme();
+    }
+
+    toggleTheme() {
+        const theme = this.state.theme === 'dark' ? 'light' : 'dark';
+        this.setState({ theme });
+        
+        document.documentElement.classList.add('theme-transition');
+        document.documentElement.setAttribute('data-theme', theme);
+        window.setTimeout(function() {
+        document.documentElement.classList.remove('theme-transition')
+        }, 1000);
+    }
+
     render() {
         return (
             <div className="theme-switch-wrapper">
                 <label className="theme-switch" htmlFor="checkbox">
-                    <input type="checkbox" id="checkbox" />
+                    <input type="checkbox" id="checkbox" onChange={this.toggleTheme}/>
                     <div className="slider round"></div>
                 </label>
             </div>
